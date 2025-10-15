d82c44de736ba9411da1dd27f72132885a604046
Uriel Velazquez <evelazqueza1900@alumno.ipn.mx>
2025-10-14 20:07:53 -0600

    VF
    Final Version

--- practica1/mochila.py
+++ practica1/mochila.py
@@ -1,10 +1,12 @@
+
+#Practica realizada por  Rodríguez Juárez Héctor Sajoni y Velazquez Arrieta Eduardo Uriel 
 import random
 import numpy as np
 #delimitamos las variables para no salirnos de los rangos del problema 
 n_genes = 7
 n_individuos = 10
 tot_ob= 10
-n_generaciones = 50
+n_generaciones = 10
 
 dulces = ["decoy_detonators" ,"love_potion", "extendable_ears" ,"skiving_snackbox" ,"fever_fudge", "puking_pastilles","nosebleed_nougat"]
 peso = [4,2,5,5,2,1.5,1]
@@ -29,16 +31,18 @@ def primera_gen():
 		generacion.append(individuo)
 	return generacion
 
-def mutacion(generacion):
-	for i in range(n_individuos):
-		if random.random() < .1: #probabilidad de alterar un gen del .1 o 10%
-			j = random.randint(0, n_genes -1) #seleccionamos un cromosoma al azar
-			if j == 1:
-				generacion[i][j] = random.randint(3,tot_ob//2) #para cumplir que se deben de llevar al menos 3 love_potion
-			elif j == 3:
-				generacion[i][j] = random.randint(2,tot_ob//2) #para cumplir que se deben de llevar al menos 2 skiving_snackbox
-			else:
-				generacion[i][j] = random.randint(1,tot_ob//2)
+def mutacion(ind):
+	if random.random() < .1: #probabilidad de alterar un gen del .1 o 10%
+		j = random.randint(0, n_genes -1) #seleccionamos un cromosoma al azar
+		#capacidad = np.dot(ind, pesos) // pesos[j]
+		capacidad = int(max(0, (30 - np.dot(ind, peso)) // peso[j]))
+
+		if j == 1:
+			ind[j] = random.randint(3,capacidad) if capacidad >= 4 else 3
+		elif j == 3:
+			ind[j] = random.randint(2,capacidad) if capacidad >= 4 else 2
+		else:
+			ind[j] = random.randint(0,capacidad) if capacidad >= 1 else 0
 
 def get_fitness(cromosoma):
 	if cromosoma[1] < 3 or cromosoma[3] < 2 or np.dot(peso,cromosoma) > 30:
@@ -60,7 +64,6 @@ def girar_ruleta(ruleta):
 	i = 0
 	while i < len(ruleta) and ruleta[i] < r: 
 		i += 1
-	print(i)
 	return i
 
 def reproducir(padre1, padre2):
@@ -75,6 +78,15 @@ def reproducir(padre1, padre2):
 			hijo2[i] = aux
 	return hijo1, hijo2
 
+def torneo(ind1, ind2, ind3, ind4):
+	m = {1: ind1, 2: ind2, 3: ind3, 4: ind4}
+	l = [(get_fitness(ind1), 1)]
+	l.append((get_fitness(ind2), 2))
+	l.append((get_fitness(ind3), 3))
+	l.append((get_fitness(ind4), 4))
+	l.sort()
+	return m[l[0][1]], m[l[1][1]]
+
 if __name__ == '__main__':
 	actual_gen = primera_gen()
 	
@@ -82,12 +94,10 @@ if __name__ == '__main__':
 	best = []
 	best_fit = 0
 	
-	for _ in range(n_generaciones):
+	for k in range(n_generaciones):
 		ruleta = get_ruleta(actual_gen)
-		print('\n1', ruleta)
 		next_gen = []
 		for _ in range(n_individuos//2):
-			print
 			i = girar_ruleta(ruleta)
 			j = i
 			while j == i:
@@ -96,6 +106,9 @@ if __name__ == '__main__':
 			ind1, ind2 = [], []
 			if random.random() < .85:
 				ind1, ind2 = reproducir(actual_gen[i], actual_gen[j])
+				mutacion(ind1)
+				mutacion(ind2)
+				ind1, ind2 = torneo(ind1, ind2, actual_gen[i], actual_gen[j])
 			else:
 				ind1 = actual_gen[i]
 				ind2 = actual_gen[j]
@@ -106,11 +119,13 @@ if __name__ == '__main__':
 				best = ind1.copy()
 				best_fit = get_fitness(ind1)
 			if get_fitness(ind2) > best_fit and np.dot(ind2, peso) <= 30:
-				best = ind1.copy()
+				best = ind2.copy()	
 				best_fit = get_fitness(ind2)
+			
 		actual_gen = next_gen
-		mutacion(actual_gen)
+		print(f"La generacion es {k}: \n {actual_gen}")		
 
+	print(f"La ultima generacion son: \n {actual_gen}")
 	print(f'\nEl mejor fit es: {best_fit} \n' )
 	print('De la mochila con los siguientes dulces. \n')
 	for i in range(len(best)):
