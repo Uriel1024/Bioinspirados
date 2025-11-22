121617445434a03482aa82422f91cf7cbdc65e18
Uriel Velazquez <evelazqueza1900@alumno.ipn.mx>
2025-11-22 16:03:05 -0600

    v1.3
    Now with the visited nodes and the sum of all the ants

--- practica4/main.py
+++ practica4/main.py
@@ -1,11 +1,12 @@
 import random
-def reciproco(matriz):
+import copy
+def reciproco(matriz,q):
 	for i in range(len(matriz)):
 		for j in range(len(matriz[0])):
 			if matriz[i][j] == 0:
 				continue
 			else: 
-				matriz[i][j] = 1/matriz[i][j]
+				matriz[i][j] = q/matriz[i][j]
 	return matriz
 
 def calc_suma(feromonas,dis_inv,a,b,n):
@@ -15,56 +16,89 @@ def calc_suma(feromonas,dis_inv,a,b,n):
 			suma[i] += (feromonas[i][j]**a)*(dis_inv[i][j]**b)
 	return suma
 
-def get_ruleta(proba):
-	ruleta = []
-	fit = 0
-	for individuo in proba:
-		fit += proba[i]
-		ruleta.append(fit)
-	return ruleta
+def fitness(nodosv,distancias):
+	dist_total = []
+	for camino_h in nodosv:
+		hormiga = 0
+		for i in range(len(camino_h)-1):
+			nodo_inicio = camino_h[i]
+			nodo_fin = camino_h[i+1]
+			hormiga += distancias[nodo_inicio][nodo_fin]
+		dist_total.append(hormiga)
+	return dist_total
 
-def girar_ruleta(ruleta):
-	total = ruleta[-1]
-	r = random.uniform(0,total)
-	i = 0
-	while i < len(ruleta) and ruleta[i] < r:
-		i += 1
-	return i
 
-distancias  = ([[0,6,9,17,13,21],[6,0,19,21,12,18]
-	,[9,19,0,20,23,11],[17,21,20,0,15,10],
-	[13,12,23,15,0,21],[21,18,11,10,21,0]])
-n = len(distancias)
-feromonas = [[.1 for _ in range(n)] for _ in range(n)]
-caminos = [[0.0 for _ in range(n)]for _ in range(n)]
-proba= [[0.0 for _ in range(n)]for _ in range(n)]
-dis_inv = reciproco(distancias)
-p,q,a,b= .2,1,1.5,.8
-ite = 0 
-tot = [0] * n
-best = [0] * n
-while ite < 10:
-	suma = calc_suma(feromonas,dis_inv,a,b,n)
+def actualizar_feromonas(feromonas,nodsv,distancias,a,b,p):
+		print(feromonas)	
+
+def definir_rutas(suma,n):
 	for i in range(n):
 		if suma[i] == 0.0:
 			continue
 		for j in range(n):
 			proba[i][j] = (feromonas[i][j]**a) * (dis_inv[i][j]**b)/ suma[i]
-	canmino_actual = [[i] for i in range(n)]
+	canmino_actual = [[k]for k in range(n)]
 	for k in range(n):
 		actual = k 
 		nodosv = [k]
 		while len(nodosv) < n:
-			proba_actual = proba[k]
-			ruleta = get_ruleta(proba_actual)
-			siguiente_nodo = girar_ruleta(ruleta)
-			if siguiente_nodo == -1:
+			proba_disponible = [0.0] * n
+
+			suma_disponible = 0.0
+			for j in range(n):
+				if j not in nodosv:
+					proba_disponible[j] = proba[actual][j]
+					suma_disponible += proba_disponible[j]
+			if suma_disponible == 0:
 				break
-			canmino_actual[actual].append(siguiente_nodo)
+			proba_ruleta = []
+			nodos_elegibles = []
+			for j in range(n):
+				if j not in nodosv:
+					proba_ruleta.append(proba_disponible[j] / suma_disponible)
+					nodos_elegibles.append(j)
+			ruleta = []
+			fit = 0
+			for p_val in proba_ruleta:
+				fit += p_val
+				ruleta.append(fit)
+
+			r = random.uniform(0,ruleta[-1])
+			idx = 0
+			while idx < len(ruleta) and ruleta[idx] < r:
+				idx += 1
+			siguiente_nodo = nodos_elegibles[idx]
+			canmino_actual[k].append(siguiente_nodo)
 			nodosv.append(siguiente_nodo)
 			nodo_actual = siguiente_nodo
+		#para regresar al nodo de inicio 
+		if len(canmino_actual[k]) == n:
+			canmino_actual[k].append(k)	
+	return canmino_actual
 
+distancias  = ([[0,6,9,17,13,21],[6,0,19,21,12,18]
+	,[9,19,0,20,23,11],[17,21,20,0,15,10],
+	[13,12,23,15,0,21],[21,18,11,10,21,0]])
+
+dis_inv = [x[:] for x in distancias]
+p,q,a,b= .2,1,1.5,.8
+n = len(distancias)
+feromonas = [[.1 for _ in range(n)] for _ in range(n)]
+caminos = [[0.0 for _ in range(n)]for _ in range(n)]
+proba= [[0.0 for _ in range(n)]for _ in range(n)]
+dis_inv = reciproco(dis_inv,q)
+ite = 0 
+tot = [0] * n
+best = [0] * n
+
+
+while ite < 1:
+	suma = calc_suma(feromonas,dis_inv,a,b,n)
+	canmino_actual = definir_rutas(suma,n)
+	print(canmino_actual)
+	fit = fitness(canmino_actual,distancias)
+	print(fit)
 	ite += 1
-	print(f"La {ite} iteracion.\n")
+
 
 print(f"El mejor camino que se encontró es: ")
